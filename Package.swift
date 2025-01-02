aaf45f9359f8f02db3eb29fe2904b73e1bdc0938
Michael Redig <mredig@gmail.com>
2025-01-01 23:25:13 -0600

    (refactor) rename package name

--- Package.swift
+++ Package.swift
@@ -4,18 +4,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "LocalSQLite",
+    name: "SQLite3",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "LocalSQLite",
-            targets: ["LocalSQLite"]),
+            name: "SQLite3",
+            targets: ["SQLite3"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LocalSQLite"),
-
+            name: "SQLite3"),
     ]
 )
